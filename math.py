9e675f9c099b1c850888733948ba93c906763157
LENOVO <antara_agarwal@persistent.com>
2021-05-28 14:46:15 +0530

    Added function headers

--- math.py
+++ math.py
@@ -1,8 +1,12 @@
+#Add implement
 def add(x,y):
     pass
+  #Subtract implement  
 def subtract(x,y):
     pass
+#mult implement
 def multiply(x,y):
     pass
+#Divide implement
 def divide(x,y):
     pass
